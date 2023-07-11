a7d2c996bd5a2887f560180a62a463d111e6d841
OshadiSavidya <99028887+OshadiSavidya@users.noreply.github.com>
2023-07-11 21:25:40 +0530

    new project

---
+++ new_project/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';  
+import 'package:new_project/pages/home_page.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'session_project',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: home_page(),
+    );
+  }
+}
